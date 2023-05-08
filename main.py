49861c0ab9e92c43994f4feb5c45a47250222de0
Vitor Rafael <rqfvitor@gmail.com>
2023-05-08 08:45:58 -0300

    build: renname main file

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def create_message(is_good):
 tweeted_at = None
 
 
-@scheduler.scheduled_job("interval", hours=2)
+@scheduler.scheduled_job("interval", minutes=10)
 def checker():
     global tweeted_at
 
